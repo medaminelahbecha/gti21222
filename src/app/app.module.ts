862c44966b59d013375d81017bc9f39100cde694
aymen2782 <aymen.sellaouti@gmail.com>
2022-02-22 08:19:36 +0100

    color component exercice binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
 import { ThirdComponent } from './components/third/third.component';
+import { ColorComponent } from './components/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponent,
-    ThirdComponent
+    ThirdComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
