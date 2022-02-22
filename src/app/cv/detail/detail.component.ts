b4ac5271e5adcf5c06fa6145ea4c61286f8e42b0
aymen2782 <aymen.sellaouti@gmail.com>
2022-02-22 11:09:37 +0100

    cvTech first sprint

---
+++ src/app/cv/detail/detail.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Cv} from "../model/cv";
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+export class DetailComponent implements OnInit {
+  // detailComponent(cv) {}
+  @Input() cv: Cv | null = null;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/cv/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Cv} from "../model/cv";
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  cvs: Cv[] = [];
+  constructor() {
+    this.cvs = [
+      new Cv(1, 'sellaouti', 'aymen', 'teacher', 'rotating_card_profile2.png', '1234', 39),
+      new Cv(2, 'zayani', 'ahmed', 'ingénieur', 'rotating_card_profile3.png', '1235', 20),
+      new Cv(3, 'souidi', 'safa', 'ingénieur', 'rotating_card_profile.png', '1236', 20),
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
